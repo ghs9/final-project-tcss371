e40deb102af042517b4a1189a85f0481e50c91fc
Amy Irving <amy.eli.irving@gmail.com>
2016-05-17 15:42:47 -0700

    Small additions to controller.c
    Still just setting up the order of operations in controller.c

--- src/controller.c
+++ src/controller.c
@@ -29,8 +29,12 @@ int controller_main() {
     CPU_p cpu = malloc_cpu();
     cpu_dump(cpu);
 
-//    Byte opcode, rd, rs, immed7;
-//    Register branch_taken_addr;
+    Byte opcode;
+    Byte rd, sr1, sr2;
+    Byte immed5, immed6;
+    Byte PCoffset9, PCoffset11;
+    Byte vect8;
+    Register branch_taken_addr;
 
     //Temp value, will get changed later.
     int instruction = 0;
@@ -39,42 +43,62 @@ int controller_main() {
     switch (state) {
     case FETCH:
         //Sets MAR to PC.
-//        cpu_set_mar(cpu, cpu_get_pc(cpu));
+        cpu_set_mar(cpu, cpu_get_pc(cpu));
         //increment PC
-//        cpu_set_pc(cpu, cpu_get_pc(cpu)++);
+        int pc = cpu_get_pc(cpu);
+        cpu_set_pc(cpu, pc++);
 
         //Set IR to equal mem[MAR]
+        //cpu_set_ir(cpu, );
         //cpu->ir = mem[cpu->mar];
+
         state = DECODE;
         break;
     case DECODE:
-
 
         switch (instruction) {
         case INS_IMMED5:
             //        opcode = cpu_get_ir(cpu);
             //        rd = getRd(cpu);
-            //        rs = getRs(cpu);
-            //        immed7 = getImmed(cpu);
-            //        cpu->sext = sext(immed7);
+            //        sr1 = getRs(cpu);
+            //        immed5 = getImmed(cpu);
+            //        cpu->sext = sext(immed5);
 
             break;
         case INS_IMMED6:
+            //        opcode = cpu_get_ir(cpu);
+            //        rd = getRd(cpu);
+            //        sr1 = getRs(cpu);
+            //        immed6 = getImmed(cpu);
+            //        cpu->sext = sext(immed6);
 
             break;
         case INS_RS2:
+            //        opcode = cpu_get_ir(cpu);
+            //        rd = getRd(cpu);
+            //        sr1 = getRs(cpu);
+            //        sr2
 
             break;
         case INS_BR:
+            //        opcode = cpu_get_ir(cpu);
+            //        PCoffset9 =
 
             break;
         case INS_PCOFF9:
+            //        opcode = cpu_get_ir(cpu);
+            //        rd = getRd(cpu);
+            //        PCoffset9 =
 
             break;
         case INS_PCOFF11:
+            //        opcode = cpu_get_ir(cpu);
+            //         PCoffset11 =
 
             break;
         case INS_VECT8:
+            //        opcode = cpu_get_ir(cpu);
+            //         vect8
 
             break;
         }
@@ -82,22 +106,29 @@ int controller_main() {
         state = EVAL_ADDR;
         break;
     case EVAL_ADDR:
+
         // compute effective address, e.g. add sext(immed7) to register
         switch (OPC) {
-        case OPCODE_ADD:
-            break;
-        case OPCODE_AND:
+
+        case OPCODE_ADD:     // addresses of dest and src registers already available
+        case OPCODE_AND:     // addresses of dest and src registers already available
             break;
         case OPCODE_BR:
             break;
         case OPCODE_JMP:
         case OPCODE_JSR:
         case OPCODE_LD:
-        case OPCODE_LDI:
+            //cpu->mar = cpu->reg_file[rs] + cpu->sext;   // compute effective address
+            break;
+        case OPCODE_LDI:   // addresses of dest register already available
+            break;
         case OPCODE_LDR:
         case OPCODE_LEA:
         case OPCODE_NOT:
+            break;
         case OPCODE_ST:
+            //cpu->mar = cpu->reg_file[rd] + cpu->sext; // compute effective address
+            break;
         case OPCODE_STI:
         case OPCODE_TRAP:
         case OPCODE_STR:
@@ -108,13 +139,49 @@ int controller_main() {
         break;
     case FETCH_OP:
 
+        // get operands out of registers into A, B of ALU
+        // or get memory for load instr.
+        switch (OPC) {
+        case OPCODE_ADD:
+            //pu->alu->a = cpu->reg_file[rd]; // get first operand
+            //cpu->alu->b = cpu->reg_file[rs];    // get second operand
+            break;
+        case OPCODE_AND:
+            break;
+        case OPCODE_BR:
+            break;
+        case OPCODE_JMP:
+            break;
+        case OPCODE_JSR:
+            break;
+        case OPCODE_LD:
+            //cpu->mdr = mem[cpu->mdr];
+            break;
+        case OPCODE_LDI:
+            //cpu->mdr = cpu->sext;
+            break;
+        case OPCODE_LDR:
+        case OPCODE_LEA:
+        case OPCODE_NOT:
+            break;
+        case OPCODE_ST:
+            //cpu->mdr = cpu->reg_file[rs];
+            break;
+        case OPCODE_STI:
+        case OPCODE_TRAP:
+        case OPCODE_STR:
+            break;
+        }
+
         state = EXECUTE;
         break;
     case EXECUTE:
         switch (OPC) {
         case OPCODE_ADD:
+            //alu add(), store result in alu_r
             break;
         case OPCODE_AND:
+            //alu and(), store result in alu_r
             break;
         case OPCODE_BR:
             break;
@@ -124,7 +191,10 @@ int controller_main() {
         case OPCODE_LDI:
         case OPCODE_LDR:
         case OPCODE_LEA:
+            break;
         case OPCODE_NOT:
+            //alu not(), store result in alu_r
+            break;
         case OPCODE_ST:
         case OPCODE_STI:
         case OPCODE_TRAP:
