0fa6782361d5d1b19ee5f5e293873d982cba81a2
Amy Irving <amy.eli.irving@gmail.com>
2016-05-19 11:31:28 -0700

    Added Getter and setter methods to cpu_alu.c
    The functions were in cpu_alu.h but weren't implemented in cpu_alu.c
    yet.

--- src/cpu_alu.c
+++ src/cpu_alu.c
@@ -10,40 +10,59 @@
 #include "util.h"
 
 struct cpu_alu_t {
-  Register a, b, r;
+    Register a, b, r;
 };
 
-void add(CPU_ALU_p alu){
+void add(CPU_ALU_p alu) {
 
 }
 
-int subtract(){
+int subtract() {
 
 }
 
-int multiply(){
+int multiply() {
 
 }
 
-int and(){
+int and() {
 
 }
 
-int nand(){
+int nand() {
 
+}
+
+Register cpu_alu_set_a(CPU_ALU_p alu, Register value) {
+    return alu->a = value;
+}
+Register cpu_alu_get_a(CPU_ALU_p alu) {
+    return alu->a;
+}
+Register cpu_alu_set_b(CPU_ALU_p alu, Register value) {
+    return alu->b = value;
+}
+Register cpu_alu_get_b(CPU_ALU_p alu) {
+    return alu->b;
+}
+Register cpu_alu_set_r(CPU_ALU_p alu, Register value) {
+    return alu->r = value;
+}
+Register cpu_alu_get_r(CPU_ALU_p alu) {
+    return alu->r;
 }
 
 CPU_ALU_p malloc_cpu_alu() {
-  return calloc(1, sizeof(CPU_ALU_s));
+    return calloc(1, sizeof(CPU_ALU_s));
 }
 
 void free_cpu_alu(CPU_ALU_p alu) {
-  free(alu);
+    free(alu);
 }
 
 void cpu_alu_dump(CPU_ALU_p alu) {
-  printf("ALU: \n");
-  print_hex("a", alu->a);
-  print_hex("b", alu->b);
-  print_hex("r", alu->r);
+    printf("ALU: \n");
+    print_hex("a", alu->a);
+    print_hex("b", alu->b);
+    print_hex("r", alu->r);
 }
--- src/cpu_alu.h
+++ src/cpu_alu.h
@@ -14,10 +14,11 @@
 CPU_ALU_p malloc_cpu_alu();
 void free_cpu_alu(CPU_ALU_p alu);
 
-Register cpu_alu_set_a(CPU_ALU_p alu);
+Register cpu_alu_set_a(CPU_ALU_p alu, Register value);
 Register cpu_alu_get_a(CPU_ALU_p alu);
-Register cpu_alu_set_b(CPU_ALU_p alu);
+Register cpu_alu_set_b(CPU_ALU_p alu, Register value);
 Register cpu_alu_get_b(CPU_ALU_p alu);
+Register cpu_alu_set_r(CPU_ALU_p alu, Register value);
 Register cpu_alu_get_r(CPU_ALU_p alu);
 
 void cpu_alu_dump(CPU_ALU_p ir);
